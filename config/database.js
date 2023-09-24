0aa1c1586ea5a3b426e7b317d46e693390c167bb
张鲱 <rocinante1453@gmail.com>
2023-09-24 15:07:08 +1000

    :bug:modify database.js, try to fix the bug about no pg_hba.conf entry for host

--- config/database.js
+++ config/database.js
@@ -2,12 +2,21 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env(
+        "DATABASE_HOST",
+        "strapi-database-0924.cukwk65vlm3s.ap-southeast-2.rds.amazonaws.com"
+      ),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      database: env("DATABASE_NAME", "strapi-database-0924"),
+      user: env("DATABASE_USERNAME", "strapi_db_psql"),
+      password: env("DATABASE_PASSWORD", "Gmail0511~!"),
     },
     useNullAsDefault: true,
+  },
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
