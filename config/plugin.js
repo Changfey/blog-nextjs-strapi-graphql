28a297254971fcce8f444ec8667215e449d912f5
张鲱 <rocinante1453@gmail.com>
2023-09-30 20:07:20 +1000

    tweak plugin.js

--- config/plugin.js
+++ config/plugin.js
@@ -1,40 +1,59 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        s3Options: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_ACCESS_SECRET"),
-          region: env("AWS_REGION"),
-          params: {
-            Bucket: env("AWS_BUCKET_NAME"),
+module.exports = ({ env }) => {
+  const environment = env("NODE_ENV", "local");
+  const globalConfig = {
+    scheduler: {
+      enabled: true,
+      config: {
+        model: "scheduler",
+      },
+    },
+    graphql: {
+      config: {
+        endpoint: "/graphql",
+        shadowCRUD: true,
+        playgroundAlways: true,
+        depthLimit: 20,
+        amountLimit: 100,
+        apolloServer: {
+          introspection: true,
+          tracing: false,
+        },
+      },
+    },
+    "import-export-entries": {
+      enabled: true,
+    },
+  };
+
+  if (environment === "production") {
+    return {
+      ...globalConfig,
+      upload: {
+        config: {
+          provider: "aws-s3",
+          providerOptions: {
+            s3Options: {
+              accessKeyId: env("AWS_ACCESS_KEY_ID"),
+              secretAccessKey: env("AWS_ACCESS_SECRET"),
+              region: env("AWS_REGION"),
+              params: {
+                Bucket: env("AWS_BUCKET_NAME"),
+              },
+            },
+          },
+          // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
+          actionOptions: {
+            upload: {
+              ACL: null,
+            },
+            uploadStream: {
+              ACL: null,
+            },
           },
         },
       },
-      // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
-      actionOptions: {
-        upload: {
-          ACL: null,
-        },
-        uploadStream: {
-          ACL: null,
-        },
-      },
-    },
-  },
-  graphql: {
-    enabled: true,
-    config: {
-      endpoint: "/graphql",
-      shadowCRUD: true,
-      playgroundAlways: true,
-      depthLimit: 10,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-        introspection: true,
-      },
-    },
-  },
-});
+    };
+  } else {
+    return globalConfig;
+  }
+};
